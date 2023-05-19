70d3e4dfae32770a725ccdae0246714c9ee6384a
Jack Watters <johnwatt@usc.edu>
2023-05-18 23:54:02 -0700

    Theme for styled components.

---
+++ src/styles/styledComponents/theme.tsx
@@ -0,0 +1,34 @@
+// Type: Styled Component Theme
+// Description: Theme for styled components
+// Docs: https://styled-components.com/docs/advanced#theming
+import { css } from 'styled-components';
+
+const theme = {
+  colors: {
+    textPrimary: 'rgba(255, 255, 255, 0.81)',
+    textSecondary: 'rgb(127, 127, 127)',
+    backgroundPrimary: 'rgb(25, 25, 25)',
+    backgroundSecondary: 'rgb(47, 47, 47)',
+    borderColor: 'rgba(255, 255, 255, 0.282)',
+  },
+  hoverBackground: 'rgb(55, 55, 55);',
+
+  shadow: css`
+    box-shadow: rgb(15 15 15 / 15%) 0px 0px 0px 2px, rgb(15 15 15 / 15%) 0px 3px 6px;
+  `,
+
+  hoverStyle: css`
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.055);
+      border-radius: 4px;
+    }
+  `,
+
+  hoverNoBorder: css`
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.055);
+    }
+  `,
+};
+
+export default theme;
