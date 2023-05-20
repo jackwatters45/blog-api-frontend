41aa65a11133dd8c3a1fb2be9fcc922125316b16
Jack Watters <johnwatt@usc.edu>
2023-05-19 17:08:08 -0700

    Created helpers for formatting post data.

---
+++ src/utils/formatPostData.tsx
@@ -0,0 +1,34 @@
+import IUser from '../../types/user.d';
+
+const checkIfUserPopulatedAndValid = (author: Partial<IUser> | string | null) => {
+  if (typeof author === 'string')
+    throw new Error('Author as a string is not allowed for this function.');
+  return author && author._id && author.firstName && author.lastName;
+};
+
+export const getAuthorFullName = (author: null | string | Partial<IUser>) => {
+  const authorIsValid = checkIfUserPopulatedAndValid(author);
+  if (!authorIsValid) return 'Unknown';
+  const { firstName, lastName } = author as IUser;
+  return `${firstName} ${lastName}`;
+};
+
+export const getAuthorId = (author: Partial<IUser> | string | null) => {
+  const authorIsValid = checkIfUserPopulatedAndValid(author);
+  if (!authorIsValid) return false;
+
+  const { _id } = author as IUser;
+  return _id;
+};
+
+export const formatDate = (date: string) => {
+  const dateObj = new Date(date);
+  const month = dateObj.toLocaleString('default', { month: 'long' });
+  const day = dateObj.getDate();
+  return `${month} ${day}`;
+};
+
+export const formatContent = (content: string) => {
+  if (content.length > 200) return `${content.slice(0, 200)}...`;
+  return content;
+};
