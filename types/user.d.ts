f76e4ae2a216c9dbb6a9d05829611d7d9ceab334
Jack Watters <johnwatt@usc.edu>
2023-05-19 17:07:32 -0700

    Added fullname to user interface.

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,8 +1,9 @@
 interface IUser {
-  id: string;
+  _id: string;
   userType: string;
   firstName: string;
   lastName: string;
+  fullName?: string;
   username: string;
   email: string;
   password: string;
